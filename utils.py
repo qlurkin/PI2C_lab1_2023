6fccd6da15538cc3d78fc75181684c11f695f65e
Quentin Lurkin <qlurkin@gmail.com>
2023-02-27 11:42:14 +0100

    Utils implementation

--- utils.py
+++ utils.py
@@ -1,3 +1,6 @@
+import math
+from scipy import integrate as itg
+
 def fact(n):
 	"""Computes the factorial of a natural number.
 	
@@ -5,7 +8,14 @@ def fact(n):
 	Post: Returns the factorial of 'n'.
 	Throws: ValueError if n < 0
 	"""
-	pass
+
+	if n < 0:
+		raise ValueError("n must be positive")
+	
+	res = 1
+	for i in range(1, n+1):
+		res *= i
+	return res
 
 def roots(a, b, c):
 	"""Computes the roots of the ax^2 + bx + x = 0 polynomial.
@@ -14,7 +24,13 @@ def roots(a, b, c):
 	Post: Returns a tuple with zero, one or two elements corresponding
 		to the roots of the ax^2 + bx + c polynomial.
 	"""
-	pass
+	delta = b**2 - 4*a*c
+	if delta < 0:
+		return tuple()
+	if delta == 0:
+		return (-b/2/a,)
+	sqrtDelta = math.sqrt(delta)
+	return ((-b - sqrtDelta)/2/a, (-b + sqrtDelta)/2/a)
 
 def integrate(function, lower, upper):
 	"""Approximates the integral of a fonction between two bounds
@@ -29,9 +45,9 @@ def integrate(function, lower, upper):
 		you'll probably need the 'eval' function to evaluate the function
 		to integrate given as a string.
 	"""
-	pass
+	return itg.quad(lambda x: eval(function, {'x': x}), lower, upper)[0]
 
 if __name__ == '__main__':
 	print(fact(5))
-	print(roots(1, 0, 1))
+	print(roots(2, 1, -2))
 	print(integrate('x ** 2 - 1', -1, 1))
